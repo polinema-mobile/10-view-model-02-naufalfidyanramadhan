6f65a14746827ab35f9de845bcd5745d1decaf5c
naufalfidyanramadhan <54935087+naufalfidyanramadhan@users.noreply.github.com>
2020-11-06 01:59:29 -0800

    viewModel02

--- app/src/main/java/id/putraprima/mvvmlogin/fragments/SplashFragment.java
+++ app/src/main/java/id/putraprima/mvvmlogin/fragments/SplashFragment.java
@@ -2,15 +2,23 @@ package id.putraprima.mvvmlogin.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import id.putraprima.mvvmlogin.R;
+import id.putraprima.mvvmlogin.databinding.FragmentSplashBinding;
 
 public class SplashFragment extends Fragment {
+    Handler handler;
+    FragmentSplashBinding binding;
 
     public SplashFragment() {
         // Required empty public constructor
@@ -21,5 +29,19 @@ public class SplashFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Navigation.findNavController(view).navigate(R.id.action_splashFragment_to_loginFragment);
+            }
+        };
+
+        handler.postDelayed(runnable, 3000);
     }
 }
